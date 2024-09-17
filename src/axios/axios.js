6699b3e4bdbbdcb48a11fe579a55375b2693234c
unknown <ffadilah682@gmail.com>
2024-09-17 18:00:47 +0800

    fix

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const Axios = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://backendjqueen.4muda.co/api",
   withCredentials: true,
 });
 
